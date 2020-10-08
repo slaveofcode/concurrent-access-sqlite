3c67ed6b7044ac931fd3a18604052243a993cd07
Kresna <slaveofcode@gmail.com>
2020-10-08 14:33:55 +0700

    Added delay on database locking error

--- process/process.go
+++ process/process.go
@@ -1,7 +1,10 @@
 package process
 
 import (
+	"errors"
+	"log"
 	"math/rand"
+	"os"
 	"strconv"
 	"time"
 
@@ -30,7 +33,7 @@ func connDB(dbPath string) *gorm.DB {
 	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 
 	if err != nil {
-		panic("[Process One] Unable to open database connection: " + err.Error())
+		panic("Unable to open database connection: " + err.Error())
 	}
 
 	return db
@@ -38,23 +41,50 @@ func connDB(dbPath string) *gorm.DB {
 
 func readProduct(db *gorm.DB, name string) *models.Product {
 	var pdt models.Product
-	db.Where("name = ?", name).First(&pdt)
+	err := db.Where("name = ?", name).First(&pdt).Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		log.Println("Got error when selecting product " + name + ": " + err.Error())
+	}
 	return &pdt
 }
 
 func writeProduct(db *gorm.DB, p *models.Product) {
-	print("Will create new Product" + p.Name)
+	exec := 0
+	interupted := false
+	for exec < 1 {
+		err := db.Create(p).Error
+		if err != nil {
+			// interupted = true
+			log.Println("Got error when creating new product" + p.Name + ": " + err.Error())
+			log.Println("Retrying...")
+			time.Sleep(time.Second * 5)
+		} else {
+			exec = 1
+		}
+	}
 
-	err := db.Create(p).Error
-	if err != nil {
-		panic("Got error when creating new product" + p.Name + ": " + err.Error())
+	if interupted {
+		os.Exit(1)
 	}
 }
 
 func updateProduct(db *gorm.DB, p *models.Product) {
-	err := db.Model(p).Update("name", p.Name+" - Updated").Error
-	if err != nil {
-		panic("Got error when updating product" + p.Name + ": " + err.Error())
+	exec := 0
+	interupted := false
+	for exec < 1 {
+		err := db.Model(p).Update("name", p.Name+" - Updated").Error
+		if err != nil {
+			log.Println("Got error when updating product" + p.Name + ": " + err.Error())
+			log.Println("Retrying...")
+			// interupted = true
+			time.Sleep(time.Second * 1)
+		} else {
+			exec = 1
+		}
+	}
+
+	if interupted {
+		os.Exit(1)
 	}
 }
 
@@ -74,7 +104,11 @@ func Serve(cfg *ProcessCfg) {
 
 	label = cfg.Label
 
-	for {
+	threeMinForward := time.Now().Add(time.Hour * 3)
+
+	// Loop for one minute
+	for time.Now().Before(threeMinForward) {
+
 		time.Sleep(cfg.DelayOps * time.Millisecond)
 
 		nameID := rand.Intn(10000-2) + 2
